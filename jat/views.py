27ff0cda4062173caafe6a58854a40149280ec05
choigabin <s2019w17@e-mirim.hs.kr>
2021-06-03 09:49:03 +0900

    jat/views IntroductionCreateView, IntroductionUpdateView, IntroductionDeleteView

--- jat/views.py
+++ jat/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.db.models import Max
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from django.views import generic
 
 from jat.models import Repository, Introduction
@@ -7,8 +9,41 @@ from jat.models import Repository, Introduction
 class RepositoryListView(generic.ListView):
     model = Repository
 
+
 class RepositoryDetailView(generic.DetailView):
     model = Repository
 
-class IntroductionDetailView(generic):
+
+class RepositoryCreateView(generic.CreateView):
+    model = Repository
+    fields = ['name', 'description', 'deadline']  # '__all__'
+    template_name_suffix = '_create'
+    # success_url = reverse_lazy('jat:repository_list')
+    def get_initial(self):
+        repository = get_object_or_404(Repository, pk=self.kwargs['repository_pk'])
+        introduction = repository.introduction_set.aggregate(Max('version'))
+        version = introduction['version_max'] #repository의 introduction의 최댓값
+        if version == None: #introduction이 아예 없으면, version 기본값 1
+            version = 1
+        else:               #introduction이 있으면, version 최댓값에서 +1
+            version += 1
+        return {'repository': repository, 'version': version}
+
+    def get_success_url(self):
+        return reverse_lazy('jat:repository_detail', kwargs={'pk':self.kwargs['repository_pk']})
+
+
+class RepositoryUpdateView(generic.UpdateView):
+    model = Repository
+    fields = ['name', 'description', 'deadline']  # '__all__'
+    template_name_suffix = '_update'
+    success_url = reverse_lazy('jat:repository_list')
+
+
+class RepositoryDeleteView(generic.DeleteView):
+    model = Repository
+    success_url = reverse_lazy('jat:repository_list')
+
+
+class IntroductionDetailView(generic.DetailView):
     model = Introduction
